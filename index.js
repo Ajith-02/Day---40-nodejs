14d93638e73c46afbed47c8911d4600dfec0a8b8
pajith551@gmail.com <pajith551@gmail.com>
2021-12-04 01:08:42 +0530

    Changes for Heroku

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ async function createConnection(){
 export const client = await createConnection()
 
 app.get("/", (request, response)=>{
-     response.send("Hello All");
+     response.send("Hello world");
  });
 
  app.use("/movies", moviesRouter)
